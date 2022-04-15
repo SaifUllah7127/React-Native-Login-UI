203cf5f88903045cf18ad8db6f6418bc147df35e
saifullah7127 <saif12.7127@gmail.com>
2022-04-15 12:18:50 +0500

    React Native Login UI

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, AppRegistry } from "react-native";
+import * as React from "react";
+import {
+  Provider as PaperProvider,
+  TextInput,
+  Button,
+} from "react-native-paper";
+import { name as appName } from "./app.json";
 
 export default function App() {
+  const [text, setText] = React.useState("");
+  const [pass, setPass] = React.useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>SAIFULLAH app!</Text>
+      <TextInput
+        label="Email"
+        value={text}
+        onChangeText={(text) => setText(text)}
+      >
+        {console.log(text)}
+      </TextInput>
+      <TextInput
+        label="Password"
+        value={pass}
+        onChangeText={(pass) => setPass(pass)}
+      />
+      <Button mode="contained" onPress={() => console.log("dab gaya")}>
+        Submit
+      </Button>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -13,8 +38,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    textAlign: "center",
+    backgroundColor: "#808080",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
